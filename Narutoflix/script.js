980f75114de9c1083ad595595498b6c71fa67c61
SemiConductorShortage <83015532+SemiConductorShortage@users.noreply.github.com>
2021-09-05 21:01:25 +0400

    PLS WORK???
    PLEASE???

--- Narutoflix/script.js
+++ Narutoflix/script.js
@@ -1,16 +1,20 @@
 // JavaScript source code
 
 function chooseSas() {
-    window.open('episelect.html?as=sas');
-    return(1)
+    window.location.href = 'episelect.html?as=sas';
+    return (1)
 }
 
 function chooseSak() {
-    window.open('episelect.html?as=sak');
-    return(2)
+    window.location.href = 'episelect.html?as=sak';
+    return (2)
 }
 
 function chooseNar() {
-    window.open('episelect.html?as=nar');
-    return(3)
+    window.location.href = 'episelect.html?as=nar';
+    return (3)
+}
+
+window.onload = function get_body() {
+    $0.remove()
 }

2cfa7c0f566919d27878efc00a13524f44ea4e6b
SemiConductorShortage <83015532+SemiConductorShortage@users.noreply.github.com>
2021-09-05 20:44:15 +0400

    Added Narutoflix, began working on Classkicker

---
+++ Narutoflix/script.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+
+function chooseSas() {
+    window.open('episelect.html?as=sas');
+    return(1)
+}
+
+function chooseSak() {
+    window.open('episelect.html?as=sak');
+    return(2)
+}
+
+function chooseNar() {
+    window.open('episelect.html?as=nar');
+    return(3)
+}
---
+++ script.js
@@ -0,0 +1,8 @@
+function narutoflix() {
+    window.location = "Narutoflix/index.html"
+};
+
+
+function classkicker() {
+    window.location = "Classkicker/index.html"
+};

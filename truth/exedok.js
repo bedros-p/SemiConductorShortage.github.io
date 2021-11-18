0b31a62d1e48bd4b9b441f8e774992e55f2e2565
SemiConductorShortage <83015532+SemiConductorShortage@users.noreply.github.com>
2021-11-18 15:26:47 +0400

    Sorry.

---
+++ truth/exedok.js
@@ -0,0 +1,7 @@
+function triggerwarning() {
+    confirm("This webpage might contain sensitive content.\nConsider clicking away if you feel uneasy at the mention of such topics:\n Self Harm, Fear, Psychotic Behavior and Manipulation \nOtherwise, click OK")
+}
+
+function continued() {
+    document.body.innerHTML = "<video src='apology.mp4' controls='' autoplay>"
+}
